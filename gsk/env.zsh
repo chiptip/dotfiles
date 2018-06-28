08f7934419a336a701d0582daa61a3df4b2ef33b
Sonny Kwok <sonny.x.kwok@gsk.com>
2018-06-28 10:56:59 -0400

    commented out environment variables

--- gsk/env.zsh
+++ gsk/env.zsh
@@ -1,9 +1,11 @@
 export USER=sk343407
 
+source ~/Workspaces/dotfiles/gsk/secret.zsh
+
 export PROXY_SERVER="gskproxy.gsk.com"
 export PROXY_PORT="800"
 
-export HTTP_PROXY="http://${USER}:${PASS}@${PROXY_SERVER}:${PROXY_PORT}/"
-export http_proxy=${HTTP_PROXY}
-export HTTPS_PROXY=${HTTP_PROXY}
-export https_proxy=${HTTP_PROXY}
+# export HTTP_PROXY="http://${USER}:${PASS}@${PROXY_SERVER}:${PROXY_PORT}/"
+# export http_proxy=${HTTP_PROXY}
+# export HTTPS_PROXY=${HTTP_PROXY}
+# export https_proxy=${HTTP_PROXY}
