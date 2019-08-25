379227b2bcf97ef228bb1cb0521f229e8450b8e7
Sonny Kwok <sonny.x.kwok@gsk.com>
2019-08-25 15:10:26 -0400

    added terraform plugin

---
+++ terraform/aliases.zsh
@@ -0,0 +1,11 @@
+alias tf='terraform $*'
+
+alias tfw='terraform workspace $*'
+
+alias tfi='terraform init $*'
+
+alias tfp='terraform plan $*'
+
+alias tfa='terraform apply $*'
+
+alias tfaa='terraform apply --auto-approve'
