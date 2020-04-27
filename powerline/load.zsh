0fee328507334dd755be2665dd828e885678bdd5
Sonny Kwok <sonny.x.kwok@gsk.com>
2020-04-27 08:52:37 -0400

    added powerline

---
+++ powerline/load.zsh
@@ -0,0 +1,9 @@
+export POWERLINE_CONFIG_COMMAND=~/Workspaces/powerline/scripts/powerline-config
+
+/usr/local/bin/powerline-daemon -q
+
+POWERLINE_ZSH_CONTINUATION=1
+POWERLINE_ZSH_SELECT=1
+
+# load zsh powerline bindings
+. /usr/local/lib/python3.7/site-packages/powerline/bindings/zsh/powerline.zsh
