cda565d9655e7eac7ef8fec8e9dedecaa6745249
Sonny Kwok <sonny.x.kwok@gsk.com>
2020-04-27 08:52:53 -0400

    corrected pyenv init scripts

---
+++ pyenv/load.zsh
@@ -0,0 +1,11 @@
+export PATH=$PATH:/usr/local/bin
+# https://github.com/pyenv/pyenv-virtualenvwrapper
+
+# use pyenv with virtualenvwrapper
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+
+# ensure initialation is done
+eval "$(pyenv init -)"
+
+export WORKON_HOME=$HOME/.virtualenvs
+pyenv virtualenvwrapper_lazy
